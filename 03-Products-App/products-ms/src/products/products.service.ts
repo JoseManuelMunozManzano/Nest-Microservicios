2039897d4830a2779627e90021a128d5227fc0ed
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-05-01 06:12:29 +0200

    03-Products-App - products-ms - Paginar mediante Prisma

--- 03-Products-App/products-ms/src/products/products.service.ts
+++ 03-Products-App/products-ms/src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 // El extends y el implements está explicado en: https://docs.nestjs.com/recipes/prisma
 @Injectable()
@@ -20,8 +21,24 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return this.product.findMany({});
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalPages = await this.product.count();
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        // skip empieza desde el 0 y hay que multiplicarlo por el límite.
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      },
+    };
   }
 
   findOne(id: number) {
